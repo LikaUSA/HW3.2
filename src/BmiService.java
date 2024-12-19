7507771f94a6d3cc3c2f02964b86370996be04ad
Andjela Burns <kaz_ang@yahoo.com>
2024-12-19 05:01:08 +0100

    updated parameters

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calculate(double height, double weight) {
-        height = 1.87; // рост
-        weight = 98; // вес
-        int index = (int) (weight / (height * height));
+    public int calculate(double heightM, int weightKg) {
+        heightM = 1.87; // рост
+        weightKg = 98; // вес
+        int index = (int) (weightKg / (heightM * heightM));
         return index;
     }
 }
