a95c439fe6c1d46fb63867d2a3f2b12f6d5200ce
Andjela Burns <kaz_ang@yahoo.com>
2024-12-19 01:05:06 +0100

    changed data

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate(double height, double weight) {
-        height = 1.99; // рост
-        weight = 88; // вес
+        height = 1.87; // рост
+        weight = 98; // вес
         int index = (int) (weight / (height * height));
         return index;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        int index = service.calculate(1.99, 88);
+        int index = service.calculate(1.87, 98);
         System.out.println("Индекс Массы Тела " + index);
     }
 }
